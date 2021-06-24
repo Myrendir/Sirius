46ca2425fa8fa2dcd0f3f17d3dcdcf8637d12127
Nicolas <nicoladufresne@gmail.com>
2021-06-24 10:09:56 +0200

    front accueil

--- styles/layout/NavBar/NavBar.js
+++ styles/layout/NavBar/NavBar.js
@@ -1,5 +1,15 @@
 export default theme => ({
-  title: {
-    color: 'blue'
-  }
+    title: {
+        color: 'blue'
+    }
 })
+
+function myfunction() {
+    var num = 0;
+    const nav = document.getElementsByClassName('nav-a')
+    if (window.scrollY > num) {
+        nav.classList.add("nav-green");
+    } else {
+        nav.classList.remove("nav-green");
+    }
+}
